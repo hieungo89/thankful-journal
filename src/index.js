6d3c4b72492073dfa2fe0ed84e890912881bb513
Hieu Ngo <hieu.ngo12989@gmail.com>
2024-07-22 11:01:58 -0700

    Remove comments

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDd9Vlrj5hEW15A9xCcb6-7NW6DlKoCURM",
   authDomain: "thankful-journal-43cb3.firebaseapp.com",
@@ -21,9 +14,7 @@ const firebaseConfig = {
   measurementId: "G-TBSNMW1WDP",
 };
 
-// Initialize Firebase
 initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
